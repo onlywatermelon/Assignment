0cdf9b5cc9205d7785b5e5a3d78ea8b525523a22
weilinye <weilinyy@outlook.com>
2021-03-15 12:42:50 +0800

    Update solvable.py

--- Sliding puzzle/solvable.py
+++ Sliding puzzle/solvable.py
@@ -1,8 +1,9 @@
 # If a sliding puzzle is generated at random, this may be unsolvable.
 # The solvable function will determine the sliding puzzle's solvability.
 # Ref. Wm. Woolsey Johnson, & Story, W. (1879). Notes on the "15" Puzzle. American Journal of Mathematics, 2(4), 397-404. doi:10.2307/2369492
+
 def inver(): # determine the inversion of the list.
-    for i in reversed(range(0, len(l))):
+    for i in reversed(range(0, n)):
         pairs_of_invers = bisect.bisect_left(tem_list, l[i])
         inversion += pairs_of_invers
         tem_list.insert(pairs_of_invers, l[i])
